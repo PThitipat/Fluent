3943c1c0ce5645268347085ccf1fb16e3a37d3cf
Thitipat.P <81472967+StormSKz@users.noreply.github.com>
2024-04-03 07:41:02 +0700

    Update SaveManager.lua

--- Addons/SaveManager.lua
+++ Addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "Shijin"
+	SaveManager.Folder = "ONEBAHT"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
