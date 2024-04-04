f770e6210a8150bda1cee200d342ec11ee73a8ed
Thitipat.P <81472967+StormSKz@users.noreply.github.com>
2024-04-05 02:20:13 +0700

    Update Paragraph.lua

--- src/Elements/Paragraph.lua
+++ src/Elements/Paragraph.lua
@@ -8,14 +8,24 @@ Paragraph.__index = Paragraph
 Paragraph.__type = "Paragraph"
 
 function Paragraph:New(Config)
-	assert(Config.Title, "Paragraph - Missing Title")
-	Config.Content = Config.Content or ""
+    assert(Config.Title, "Paragraph - Missing Title")
+    Config.Content = Config.Content or ""
 
-	local Paragraph = require(Components.Element)(Config.Title, Config.Content, Paragraph.Container, false)
-	Paragraph.Frame.BackgroundTransparency = 0.92
-	Paragraph.Border.Transparency = 0.6
+    local Paragraph = require(Components.Element)(Config.Title, Config.Content, Paragraph.Container, false)
+    Paragraph.Frame.BackgroundTransparency = 0.92
+    Paragraph.Border.Transparency = 0.6
 
-	return Paragraph
+    return Paragraph
+end
+
+function Paragraph:SetText(textType, newText)
+    if textType == "Title" then
+        self.Title.Text = newText
+    elseif textType == "Content" then
+        self.Content.Text = newText
+    else
+        warn("Invalid textType provided. Please provide 'Title' or 'Content'.")
+    end
 end
 
 return Paragraph
